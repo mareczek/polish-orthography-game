01f1fe0f31a1ed94529a598a50f2b82bab3b5959
Marek Pietrucha <marek@enginearch.com>
2025-03-12 19:51:46 +0100

    Fix PostCSS configuration and update homepage URL

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('postcss-flexbugs-fixes'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
 }
