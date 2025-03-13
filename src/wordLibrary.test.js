db5e23fbe4cd8b3244c8dfc77deb97f0bdf6a06e
Marek Pietrucha <marek@enginearch.com>
2025-03-13 02:13:13 +0100

    added words + updated tests

--- src/wordLibrary.test.js
+++ src/wordLibrary.test.js
@@ -7,9 +7,33 @@ describe('Word Lists Validation', () => {
     expect(words.length).toBe(uniqueWords.size);
   });
 
+  test('each word in rzWordsList exactly one "rz"', () => {
+    rzWordsList.forEach(item => {
+      const word = item.word;
+      const matches = word.match(/rz/g);
+      if (matches === null) {
+        console.log(word);
+      }
+      expect(matches).not.toBeNull();
+      expect(matches.length).toBeGreaterThan(0);
+    });
+  });
+
   test('zWordsList has no duplicate words', () => {
     const words = zWordsList.map(item => item.word);
     const uniqueWords = new Set(words);
     expect(words.length).toBe(uniqueWords.size);
+  });
+
+  test('each word in zWordsList has exactly one "ż"', () => {
+    zWordsList.forEach(item => {
+      const word = item.word;
+      const matches = word.match(/ż/g);
+      if (matches === null) {
+        console.log(word);
+      }
+      expect(matches).not.toBeNull();
+      expect(matches.length).toBeGreaterThan(0);
+    });
   });
 });
